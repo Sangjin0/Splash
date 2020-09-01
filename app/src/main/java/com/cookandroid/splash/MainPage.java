01402aa7940e89955d595c00d66a4641b3c6d51a
Sangjin0 <qak6464@naver.com>
2020-09-01 22:28:45 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/splash/MainPage.java
@@ -0,0 +1,14 @@
+package com.cookandroid.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cookandroid.splash.R;
+
+public class MainPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
